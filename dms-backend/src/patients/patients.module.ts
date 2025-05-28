2c15d706da07c8ae05f6f85d86efb53c697d8f56
Rohit Anish <rohitanish158@gmail.com>
2025-05-28 12:41:21 +0530

    added authmodule

--- dms-backend/src/patients/patients.module.ts
+++ dms-backend/src/patients/patients.module.ts
@@ -2,8 +2,9 @@ import { Module } from '@nestjs/common';
 import { PatientsService } from './patients.service';
 import { PatientsController } from './patients.controller';
 import { PrismaService } from '../prisma.service';
-
+import { AuthModule } from '../auth.module';
 @Module({
+  imports: [AuthModule], // Add AuthModule to imports
   controllers: [PatientsController],
   providers: [PatientsService, PrismaService],
   exports: [PatientsService],
