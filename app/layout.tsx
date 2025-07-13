6a9634ce099b536e28103ffc42abe829cd60e52d
Rohit Anish <rohitanish158@gmail.com>
2025-07-13 10:09:01 +0530

    changed metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,9 +2,9 @@ import type { Metadata } from 'next'
 import './globals.css'
 
 export const metadata: Metadata = {
-  title: 'v0 App',
-  description: 'Created with v0',
-  generator: 'v0.dev',
+  title: 'DMS App',
+  description: 'Created by Rohit',
+  generator: 'Rohit',
 }
 
 export default function RootLayout({
